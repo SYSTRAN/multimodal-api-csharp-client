93859ec9b419b2606f977b8152d6470740cdfd9a
Francois Breard <francois.breard@systrangroup.com>
2016-02-17 18:55:54 +0000

    Add api key & apikey.txt file checks

--- SystranClientMultimodalApiLibTests/Api/FileApiTests.cs
+++ SystranClientMultimodalApiLibTests/Api/FileApiTests.cs
@@ -23,6 +23,8 @@ namespace Systran.MultimodalClientLib.Api.Tests
         {
             client = new ApiClient("https://platform.systran.net:8904");
             Configuration.apiClient = client;
+            if (!File.Exists("../../apiKey.txt"))
+                throw new Exception("To properly run the tests, please add an apiKey.txt file containing your api key in the SystranClientMultimodalApiLibTests folder or edit the test file with your key");
             Dictionary<String, String> keys = new Dictionary<String, String>();
             string key;
             using (StreamReader streamReader = new StreamReader("../../apiKey.txt", Encoding.UTF8))
@@ -30,6 +32,8 @@ namespace Systran.MultimodalClientLib.Api.Tests
                 key = streamReader.ReadToEnd();
             }
             keys.Add("key", key); Configuration.apiKey = keys; Configuration.apiKey = keys;
+            if (keys.Count == 0)
+                throw new Exception("No api key found, please check your apiKey.txt file");
             Configuration.apiKey = keys;
             fileApi = new FileApi(Configuration.apiClient);
         }
